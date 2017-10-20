f5b3e0d0e299ea67afd510a0590f96abc597c127
ledermauss <ledermauss@users.noreply.github.com>
2017-10-20 13:42:32 +0200

    Queries a simple workspace and says hello. Returns json response

---
+++ sample.py
@@ -0,0 +1,31 @@
+import json
+from watson_developer_cloud import ConversationV1
+
+#########################
+# message
+#########################
+
+conversation = ConversationV1(
+    username='d1fe3542-a6e8-44d6-bb09-9981011100f2',
+    password='ntguGSN3Mfn6',
+    version='2017-04-21')
+
+## Returns existing workspaces. 
+# response = conversation.list_workspaces()
+# print(json.dumps(response, indent=2))
+
+# For each action, choose the corresponding workspace
+# Obtained manuallly. Could also be obtained by querying the api
+workspace_id = '6de751b6-6522-4484-a4a7-5714f0d96c33'
+
+response = conversation.message(
+    workspace_id = workspace_id,
+    message_input={
+        'text': 'Hello'
+    }
+)
+
+print(json.dumps(response, indent=2))
+
+
+
